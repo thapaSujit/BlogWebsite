c2178c48963d94c5edb178778345e8012f83c3d3
Sujit Thapa <megasuziet@gmail.com>
2023-05-06 15:32:16 +0200

    Initial commit

---
+++ Personal website/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Personal websiteConfig(AppConfig):
+    name = 'Personal website'
---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('blog', views.blog, name='blog'),
+    path('blogpost/<str:slug>', views.blogpost, name='blogpost'),
+    path('category/<str:category>', views.category, name='category'),
+    path('categories/', views.categories, name='categories'),
+    path('search/', views.search, name='search'),
+    path('thanks', views.thanks, name='thanks'),
+]
