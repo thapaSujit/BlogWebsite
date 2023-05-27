b2ea2b2d6e0141e36f65e12fdebf98dde00a98d5
Sujit Thapa <megasuziet@gmail.com>
2023-05-27 13:44:30 +0200

    trying z´to add approx time needed to read a blog.Still not 100% working

--- home/models.py
+++ home/models.py
@@ -11,6 +11,17 @@ class Blog(models.Model):
     category = models.CharField(max_length=255, default="uncategorized")
     slug = models.CharField(max_length=100)
     time = models.DateField(auto_now_add=True)
+    
+    def save(self, *args, **kwargs):
+        # Calculate the approximate time required to read the content
+        word_count = len(self.content.split())
+        average_reading_speed = 200  # Assuming an average reading speed of 200 words per minute
+        minutes_required = word_count / average_reading_speed
+
+        # Round up to the nearest minute
+        self.approx_time = str(round(minutes_required))
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
